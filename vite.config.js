daa1b12be8586383578bd054642bbd08b492164b
Jesus <jesus.ramirez.bocanegra@gmail.com>
2024-03-01 11:27:16 -0600

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://JesusRamirezB.github.io/react-rick-and-morty/",
   plugins: [react()],
 })
 
